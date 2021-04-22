2558fd7bb851cf31fe2192696605d24f24b44f06
QuangDat95 <80699149+QuangDat95@users.noreply.github.com>
2021-04-22 21:28:35 +0700

    Add files via upload

---
+++ 2.MANG_HAM_PHP/BT_tim_pt_max/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tìm max mảng 2 chiều</title>
+</head>
+<body>
+<!--form action="" method="POST">
+<p>Nhập vào mảng</p><input type="text" name="Nhap_mang" placeholder="Nhập vào mảng"/>
+<input type="submit" value="Tìm số"/>
+</form-->
+    <?php
+        $mang = [[1,2,34,5,6,7,8],[3,7,9,0,17,77,29]];
+        $max = 0;
+        for($i=0;$i<count($mang);$i++){
+            for($j=0;$j<count($mang[$i]);$j++){
+                if($mang[$i][$j]>$max){
+                    $max = $mang[$i][$j];
+                }
+            }
+        }
+        echo "Số lớn nhất trong mảng là: {$max}";
+        echo typeOf($mang);
+    ?>
+</body>
+</html>
